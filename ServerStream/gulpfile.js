ac3f797a1df6bd32a26cc6d0b81cea9ea9f98b17
sharkbyteprojects <sharkbyteprojects@protonmail.com>
2020-06-25 20:58:15 +0200

    Add Less Stylesheet

--- ServerStream/gulpfile.js
+++ ServerStream/gulpfile.js
@@ -1,13 +1,22 @@
-﻿var gulp = require("gulp");
-var babel = require("gulp-babel");
+﻿const gulp = require("gulp");
+const babel = require("gulp-babel");
 const rename = require("gulp-rename");
 const webpack = require("webpack-stream");
-var ts = require('gulp-typescript');
+const ts = require('gulp-typescript');
 const minify = require("gulp-babel-minify");
+const cleanCSS = require('gulp-clean-css');
+const less = require('gulp-less');
 const htmlmin = require("gulp-htmlmin");
 const files = ["maino.js"];
 function html() {
     return gulp.src('views/index.html').pipe(htmlmin({ collapseWhitespace: true })).pipe(rename("indexo.html")).pipe(gulp.dest('views'));
+}
+function stylesheet() {
+    return gulp.src("static/style.less")
+        .pipe(less())
+        .pipe(cleanCSS())
+        .pipe(rename("style.css"))
+        .pipe(gulp.dest("static"));
 }
 function client ()  {
     return gulp.src("static/main.js")
@@ -32,6 +41,6 @@ function server() {
         }))
         .pipe(gulp.dest("."));
 }
-exports.client = gulp.parallel(client, html);
+exports.client = gulp.parallel(client, html, stylesheet);
 exports.server = server;
-exports.default = gulp.parallel(client, server, html);
+exports.default = gulp.parallel(client, server, html, stylesheet);
